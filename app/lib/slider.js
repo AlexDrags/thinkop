ea1277d4207b06b14dda77ee0d3c9b0d85d41e9a
AlexDrags <ksander-88@yandex.ru>
2024-01-14 14:28:35 +0300

    init: init new project

---
+++ app/lib/slider.js
@@ -0,0 +1,13 @@
+// import KeenSlider from 'keen-slider';
+// const slider = new KeenSlider(
+//   '#my-slider',
+//   {
+//     loop: true,
+//     created: () => {
+//       console.log('created');
+//     },
+//   },
+//   [
+//     // add plugins here
+//   ]
+// );
